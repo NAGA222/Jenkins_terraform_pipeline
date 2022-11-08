48e6907086db284e7eb2200f6d117d922ffe3d36
“NAGA222” <bhushanam2222@gmail.com>
2022-11-08 16:03:59 +0530

    backend code added

--- variable.tf
+++ variable.tf
@@ -1,11 +1,3 @@
-variable "access_key" {
-  default = "AKIAQSQCT4WHCSS4JFIY"
-}
-
-variable "secret_key" {
-  default = "r2NzKQVb22k/mjFow3kqrtn69gkFoBucjldCFbRz"
-}
-
 variable "region" {
     default = ap-south-1
 }
