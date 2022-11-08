9936b1fefe959839f5e8ead93ac7c8d69a9ec2b7
“NAGA222” <bhushanam2222@gmail.com>
2022-11-08 14:58:24 +0530

    terraform code commit

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  default = "AKIAQSQCT4WHCSS4JFIY"
+}
+
+variable "secret_key" {
+  default = "r2NzKQVb22k/mjFow3kqrtn69gkFoBucjldCFbRz"
+}
+
+variable "region" {
+    default = ap-south-1
+}
